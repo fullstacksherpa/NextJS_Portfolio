7801c9143e2810097d2ab020d57666f8c6ec397e
fullstacksherpa <fullstacksherpa@gmail.com>
2023-09-05 06:51:51 -0700

    Banner Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,58 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import Navbar from '@/components/Navbar'
-import LeftSide from '@/components/LeftSide'
-import RightSide from '@/components/RightSide'
-import {motion} from 'framer-motion'
+"use client";
+import "./globals.css";
+import type { Metadata } from "next";
+import Navbar from "@/components/Navbar";
+import LeftSide from "@/components/LeftSide";
+import RightSide from "@/components/RightSide";
+import { motion } from "framer-motion";
+import Banner from "@/components/Banner";
 
-export const metadata: Metadata = {
-  title: 'Ongchen Sherpa',
-  description: 'Nextjs personal portfolio website powered by vercel',
-}
+//export is remove here for a begin. check for better solution.
+const metadata: Metadata = {
+  title: "Ongchen Sherpa",
+  description: "Nextjs personal portfolio website powered by vercel",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" />
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,600;1,700&display=swap" rel="stylesheet" />
-{/* {icon not working, need to fix later} */}
-{/* <link rel='icon' href={iconPath}/> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+        {/* {icon not working, need to fix later} */}
+        {/* <link rel='icon' href={iconPath}/> */}
       </head>
-      <body className='font-bodyFont w-full h-screen bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll'>
+      <body className="font-bodyFont w-full h-screen bg-bodyColor text-textLight overflow-x-hidden overflow-y-scroll">
         <Navbar />
         <div className="items-center w-full h-[88vh] xl:flex gap-20 justify-between">
-          <div className='hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0'>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.1 }}
+            className="hidden xl:inline-flex w-32 h-full fixed left-0 bottom-0">
             <LeftSide />
+          </motion.div>
+          <div className="h-[88vh] mx-auto w-full p-4 ">
+            <Banner />
           </div>
-          <div className='h-[88vh] mx-auto p-4'>middle</div>
-          <div className='hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0'>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.1 }}
+            className="hidden xl:inline-flex w-32 h-full fixed right-0 bottom-0">
             <RightSide />
-          </div>
+          </motion.div>
         </div>
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
